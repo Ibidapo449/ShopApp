630773db68197059266765d63168ecc49356ace1
Ibidapo449 <ibidapodavid10@gmail.com>
2022-04-04 15:40:21 +0100

    making cart items dismissible

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -22,48 +22,51 @@ class CartItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return 
-    // return Dismissible(
-    //   key: ValueKey(id),
-    //   background: Container(
-    //     color: Theme.of(context).errorColor,
-    //     child: Icon(
-    //       Icons.delete,
-    //       color: Colors.white,
-    //       size: 40,
-    //     ),
-    //     alignment: Alignment.centerRight,
-    //     padding: EdgeInsets.only(right: 20),
-    //     margin: EdgeInsets.symmetric(
-    //       horizontal: 15,
-    //       vertical: 4,
-    //     ),
-    //   ),
-    //   direction: DismissDirection.endToStart,
-    //   onDismissed: (direction) {
-    //     Provider.of<Cart>(context, listen: false).removeItem(productId);
-    //   },
-      Card(
+   
+        Dismissible(
+          key: ValueKey(id),
+          background: Container(
+            color: Theme.of(context).errorColor,
+                child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+             alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20),
         margin: const EdgeInsets.symmetric(
           horizontal: 15,
           vertical: 4,
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(8),
-          child: ListTile(
-            leading: CircleAvatar(
-              child: Padding(
-                padding: const EdgeInsets.all(5),
-                child: FittedBox(
-                  child: Text('\$$price'),
+          ),
+          direction: DismissDirection.endToStart,
+          onDismissed: (direction) {
+        Provider.of<Cart>(context, listen: false).removeItem(productId);
+      },
+          
+        child: Card(
+          margin: const EdgeInsets.symmetric(
+            horizontal: 15,
+            vertical: 4,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: ListTile(
+              leading: CircleAvatar(
+                child: Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: FittedBox(
+                    child: Text('\$$price'),
+                  ),
                 ),
               ),
+              title: Text(title),
+              subtitle: Text('Total: \$${(price * quantity)}'),
+              trailing: Text('$quantity x'),
             ),
-            title: Text(title),
-            subtitle: Text('Total: \$${(price * quantity)}'),
-            trailing: Text('$quantity x'),
           ),
-        ),
-      
-    );
+        
+          ),
+      );
   }
 }
