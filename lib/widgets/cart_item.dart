75c6b7ccdbaa59ed5d1f3698e448258a115b26ca
Ibidapo449 <ibidapodavid10@gmail.com>
2022-04-05 14:56:37 +0100

    showing alert dialogs

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -21,52 +21,71 @@ class CartItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return 
-   
-        Dismissible(
-          key: ValueKey(id),
-          background: Container(
-            color: Theme.of(context).errorColor,
-                child: const Icon(
+    return Dismissible(
+      key: ValueKey(id),
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: const Icon(
           Icons.delete,
           color: Colors.white,
           size: 40,
         ),
-             alignment: Alignment.centerRight,
+        alignment: Alignment.centerRight,
         padding: const EdgeInsets.only(right: 20),
         margin: const EdgeInsets.symmetric(
           horizontal: 15,
           vertical: 4,
         ),
-          ),
-          direction: DismissDirection.endToStart,
-          onDismissed: (direction) {
+      ),
+      direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+        return showDialog(
+            context: context,
+            builder: (ctx) => AlertDialog(
+                  title: const Text("Are your sure?"),
+                  content: const Text(
+                      "Do you want to remove theitem from the cart ?"),
+                  actions: [
+                    // ignore: deprecated_member_use
+                    FlatButton(
+                        onPressed: () {
+                          Navigator.of(context).pop(false);
+                        },
+                        child: const Text("No")),
+                    // ignore: deprecated_member_use
+                    FlatButton(
+                        onPressed: () {
+                          Navigator.of(context).pop(true);
+                        },
+                        child: const Text("Yes"))
+                  ],
+                ));
+      },
+      onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
-          
-        child: Card(
-          margin: const EdgeInsets.symmetric(
-            horizontal: 15,
-            vertical: 4,
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(8),
-            child: ListTile(
-              leading: CircleAvatar(
-                child: Padding(
-                  padding: const EdgeInsets.all(5),
-                  child: FittedBox(
-                    child: Text('\$$price'),
-                  ),
+      child: Card(
+        margin: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                padding: const EdgeInsets.all(5),
+                child: FittedBox(
+                  child: Text('\$$price'),
                 ),
               ),
-              title: Text(title),
-              subtitle: Text('Total: \$${(price * quantity)}'),
-              trailing: Text('$quantity x'),
             ),
+            title: Text(title),
+            subtitle: Text('Total: \$${(price * quantity)}'),
+            trailing: Text('$quantity x'),
           ),
-        
-          ),
-      );
+        ),
+      ),
+    );
   }
 }
